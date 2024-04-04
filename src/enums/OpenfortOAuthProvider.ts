0c3b8d23ab83f83e25a7b85fd037264e5ba56a26
Jaume Alavedra <jamalavedra@gmail.com>
2024-04-04 20:20:48 +0200

    add new providers

--- src/enums/OpenfortOAuthProvider.ts
+++ src/enums/OpenfortOAuthProvider.ts
@@ -2,6 +2,8 @@ export enum OpenfortOAuthProvider {
     ACCELBYTE = "accelbyte",
     FIREBASE = "firebase",
     GOOGLE = "google",
+    EMAIL = "email",
+    WALLET = "wallet",
     LOOTLOCKER = "lootlocker",
     SUPABASE = "supabase",
     PLAYFAB = "playfab",
