fd758f54907627e9584fa37c8d1d7f8cbf445ddd
gllm-dev <guillem@openfort.xyz>
2024-09-06 17:43:07 +0200

    feat: add update share method

--- src/core/ShieldSDK.ts
+++ src/core/ShieldSDK.ts
@@ -5,7 +5,6 @@ import {CustomAuthOptions} from "../models/CustomAuthOptions";
 import {NoSecretFoundError} from "../errors/NoSecretFoundError";
 import {SecretAlreadyExistsError} from "../errors/SecretAlreadyExistsError";
 import {Share} from "../models/Share";
-import {EncryptionPartMissingError} from "../errors/EncryptionPartMissingError";
 
 export class ShieldSDK {
     private readonly _baseURL: string;
@@ -28,9 +27,6 @@ export class ShieldSDK {
                     throw new NoSecretFoundError("No secret found for the given auth options");
                 }
                 const errorResponse = await response.text();
-                if (errorResponse.includes("EC_MISSING")) {
-                    throw new EncryptionPartMissingError("Encryption part missing");
-                }
                 console.error(`unexpected response: ${response.status}: ${errorResponse}`);
                 throw new Error(`unexpected response: ${response.status}: ${errorResponse}`);
             }
@@ -52,6 +48,34 @@ export class ShieldSDK {
         }
     }
 
+    public async updateSecret(auth: ShieldAuthOptions, share: Share, requestId?: string): Promise<void> {
+        try {
+            const response = await fetch(`${this._baseURL}/shares`, {
+                method: 'PUT',
+                headers: new Headers(this.getAuthHeaders(auth, requestId)),
+                body: JSON.stringify({
+                    "secret": share.secret,
+                    "entropy": share.entropy,
+                    "salt": share.encryptionParameters?.salt,
+                    "iterations": share.encryptionParameters?.iterations,
+                    "length": share.encryptionParameters?.length,
+                    "digest": share.encryptionParameters?.digest,
+                    "encryption_part": auth.encryptionPart || "",
+                    "encryption_session": auth.encryptionSession || "",
+                }),
+            });
+
+            if (!response.ok) {
+                const errorResponse = await response.text();
+                console.error(`unexpected response: ${response.status}: ${errorResponse}`);
+                throw new Error(`unexpected response: ${response.status}: ${errorResponse}`);
+            }
+        } catch (error) {
+            console.error(`unexpected error: ${error}`);
+            throw error;
+        }
+    }
+
     public async deleteSecret(auth: ShieldAuthOptions, requestId?: string): Promise<void> {
         try {
             const response = await fetch(`${this._baseURL}/shares`, {
@@ -61,9 +85,6 @@ export class ShieldSDK {
 
             if (!response.ok) {
                 const errorResponse = await response.text();
-                if (errorResponse.includes("EC_MISSING")) {
-                    throw new EncryptionPartMissingError("Encryption part missing");
-                }
                 console.error(`unexpected response: ${response.status}: ${errorResponse}`);
                 throw new Error(`unexpected response: ${response.status}: ${errorResponse}`);
             }
@@ -91,13 +112,10 @@ export class ShieldSDK {
             });
 
             if (!response.ok) {
-                const errorResponse = await response.text();
-                if (errorResponse.includes("EC_MISSING")) {
-                    throw new EncryptionPartMissingError("Encryption part missing");
-                }
                 if (response.status === 409) {
                     throw new SecretAlreadyExistsError("Secret already exists for the given auth options");
                 }
+                const errorResponse = await response.text();
                 console.error(`unexpected response: ${response.status}: ${errorResponse}`);
                 throw new Error(`unexpected response: ${response.status}: ${errorResponse}`);
             }
